3a38a131fbce40919f4925255e2e041fc5633a6f
korenev6052 <korenev6052@gmail.com>
2021-05-05 14:31:01 +0300

    saving phone to session storage

--- common.js
+++ common.js
@@ -1,11 +1,21 @@
 document.addEventListener('DOMContentLoaded', updatePhone);
 
 function updatePhone() {
-  const defaultPhone = '+79185105106';
-  const foundPhone = findPhone();
-  const phoneToUpdate = !!foundPhone ? foundPhone : defaultPhone;
   const phoneEl = document.getElementById('phone');
-  phoneEl.innerText = phoneToUpdate;
+  phoneEl.innerText = getDynamicPhone();
+}
+
+function getDynamicPhone() {
+  const foundPhone = findPhone();
+  const storageKey = 'dynamicPhone';
+  if (!!foundPhone) {
+    sessionStorage.setItem(storageKey, foundPhone);
+    return foundPhone;
+  }
+
+  const storagePhone = sessionStorage.getItem(storageKey);
+  const defaultPhone = '+79185105106';
+  return storagePhone || defaultPhone;
 }
 
 function findPhone() {
